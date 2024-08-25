a0024db5155d8b0e6c31d27f28fd9671edc9991c
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2024-08-25 01:44:53 -0700

    cleanup

--- commands/moderatoronly/permvoice.js
+++ commands/moderatoronly/permvoice.js
@@ -3,62 +3,6 @@ require('dotenv').config();
 const fs = require('node:fs');
 const Settings = require('../../Settings.js');
 
-/* function  createSettingsFile()
- * 
- * description: Creates a settings file for the guild
- * 
- * parameters: string guildId: The guild id to get the settings for
- * 
- * Returns: None
- */
-function createSettingsFile(guildId) {
-  const directoryPath = `./globalserversettings/permvoice/${guildId}`;
-  const filePath = `${directoryPath}/settings.cfg`;
-  const fileContents = ``;
-
-  try {
-    fs.mkdirSync(directoryPath, { recursive: true });
-    fs.writeFileSync(filePath, fileContents, 'utf8');
-    console.log('Settings file created successfully');
-  } catch (error) {
-    console.error('Error creating settings file:', error);
-  }
-}
-
-/* function  managePermVoiceChannels()
- * 
- * description: Creates a settings file for the guild
- * 
- * parameters: string guildId: The guild id to get the settings for
- *             string channelid: The channel id to add or delete from th settings file
- * 
- * Returns: None
- */
-function managePermVoiceChannels(guildId, channelid) {
-    const filePath = `./globalserversettings/permvoice/${guildId}/settings.cfg`;
-    
-    //Read the existing settings file
-    let fileContent = fs.readFileSync(filePath, 'utf-8');
-
-    // Split the file into lines
-    const lines = fileContent.split('/\r?\n/').filter(line => line.trim());
-    console.log(lines)
-
-    // Check if the channel id exists in the file
-    if (lines.includes(channelid)) {
-      //Remove the channel id from the file
-      const lines = fileContent.split('\n');
-      const updatedLine = lines.filter(line => line !== channelid);
-      fileContent = updatedLine.join('\n');
-    } else {
-      //Add the channel id to the file
-      fileContent += channelid + '\n';
-    }
-
-    //Write the updated file
-    fs.writeFileSync(filePath, fileContent, 'utf-8');
-}
-
 module.exports = {
   category: 'permanentvoice',
   data: new SlashCommandBuilder()
