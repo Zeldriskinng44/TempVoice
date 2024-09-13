0151534c51be214b50c7aeb05e72a757dd66f060
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2024-09-12 17:45:01 -0700

    region command

--- commands/channelcommands/region.js
+++ commands/channelcommands/region.js
@@ -6,14 +6,35 @@ module.exports = {
   category: 'channelcommands',
   data: new SlashCommandBuilder()
     .setName('region')
-    .setDescription('The region for the channel.'),
+    .setDescription('The region for the channel.')
+    .addStringOption(option =>
+			option.setName('region')
+				.setDescription('The region for the channel. Leave Empty for default.')
+				.setRequired(false)
+				.addChoices(
+					{ name: 'Brazil', value: 'brazil' },
+          { name: 'Hong Kong', value: 'hongkong' },
+          { name: 'India', value: 'india' },
+          { name: 'Japan', value: 'japan' },
+          { name: 'Rotterdam', value: 'rotterdam' },
+          { name: 'Russia', value: 'russia' },
+          { name: 'Singapore', value: 'singapore' },
+          { name: 'South Africa', value: 'south-africa' },
+          { name: 'Sydney', value: 'sydney' },
+          { name: 'US Central', value: 'us-central' },
+          { name: 'US East', value: 'us-east' },
+          { name: 'US South', value: 'us-south' },
+          { name: 'US West', value: 'us-west' },
+				)),
 async execute(interaction) {
     const guild = interaction.guild
     const member = await interaction.guild.members.fetch(interaction.user.id);
+    const voiceregion = interaction.options.getString('region');
     if (!member.voice.channel) {
       return interaction.reply({ content: 'You must be in a voice channel to use this command.', ephemeral: true });
   }
     const currentChannel = member.voice.channel.id;
+    const targetChannel = guild.channels.cache.get(currentChannel);
 
     //Check if the user is in a voice channel
     if (!channelOwners.has(currentChannel)) {
@@ -27,7 +48,18 @@ async execute(interaction) {
 
     //Method to set the channel permanet status toggles
         try {
-            await interaction.reply({ content:`This feature is a work in progress.`, ephemeral: true });
+            try {
+                await targetChannel.setRTCRegion(voiceregion);
+                console.log(voiceregion);
+                if (voiceregion === null) {
+                  await interaction.reply({ content: `The region for the channel has been set to Automatic.`, ephemeral: true });
+                } else {
+                  await interaction.reply({ content: `The region for the channel has been set to ${voiceregion}.`, ephemeral: true });
+                }
+            } catch (error) {
+              await targetChannel.setRTCRegion(null);
+                await interaction.reply({ content:`The region for the channel has been set to Automatic.`, ephemeral: true });
+            }
             
         } catch (error) {
           await interaction.reply({ content:`There was an error while using the command.`, ephemeral: true });
